40571d7c1112f47a5de89b536c9266465993498c
naoyuki42 <naoyuki.7042@outlook.jp>
2023-09-03 16:48:55 +0900

    fix: user

--- src/hooks/useState.ts
+++ src/hooks/useState.ts
@@ -1,15 +1,15 @@
 export const useState = <T>(
   initialState: T,
-  render = (..._blank: any): void => {}
+  render?: (data: T) => void
 ): [() => T, (nextState: T) => void] => {
   let data: T = initialState;
-  render(data);
+  if (render !== undefined) render(data);
   const state = () => {
     return data;
   };
   const setState = (nextState: T): void => {
     data = nextState;
-    render(state());
+    if (render !== undefined) render(state());
   };
   return [state, setState];
 };
--- src/pages/user.ts
+++ src/pages/user.ts
@@ -7,39 +7,27 @@ import {
 } from "../HTMLElement";
 
 const renderUserElement = (data: User, i: number): void => {
-  const row = createTableRowElement();
-  row.setAttribute("id", `user${i}`);
-  row.innerHTML = sanitizedText(`
-    <tr id="user${i}">
-    <td id="id${i}">${data.id}</td>
-    <td id="name${i}">${data.name}</td>
-    <td id="username${i}">${data.username}</td>
-    <td id="address${i}">${data.address.city} ${data.address.suite} ${data.address.street}</td>
-    <td id="phone${i}">${data.phone}</td>
-    <td id="company${i}">${data.company.name}</td>
-    </tr>
-  `);
-  getElement("userTable")!.appendChild(row);
+  const row = getElement(`user${i}`)!;
+  const address = `${data.address.city} ${data.address.suite} ${data.address.street}`;
+  row.innerHTML = `
+    <td id="id${i}">${sanitizedText(String(data.id))}</td>
+    <td id="name${i}">${sanitizedText(data.name)}</td>
+    <td id="username${i}">${sanitizedText(data.username)}</td>
+    <td id="address${i}">${sanitizedText(address)}</td>
+    <td id="phone${i}">${sanitizedText(data.phone)}</td>
+    <td id="company${i}">${sanitizedText(data.company.name)}</td>
+  `;
 };
 
-const updateUserElement = (data: User, i: number): void => {
-  getElement(`id${i}`)!.textContent = String(data.id);
-  getElement(`name${i}`)!.textContent = data.name;
-  getElement(`username${i}`)!.textContent = data.username;
-  getElement(
-    `address${i}`
-  )!.textContent = `${data.address.city} ${data.address.suite} ${data.address.street}`;
-  getElement(`phone${i}`)!.textContent = data.phone;
-  getElement(`company${i}`)!.textContent = data.company.name;
-};
-
-const [users, setUsers] = useState<User[]>([], (data: User[]) => {
+const [users, setUsers] = useState<User[]>([], (data: User[]): void => {
   data.map((item: User, i: number): void => {
-    if (getElement(`user${i}`) === null) {
-      renderUserElement(item, i);
-    } else {
-      updateUserElement(item, i);
+    const userRow = getElement(`user${i}`);
+    if (userRow === null) {
+      const row = createTableRowElement();
+      row.setAttribute("id", `user${i}`);
+      getElement("userTable")!.appendChild(row);
     }
+    renderUserElement(item, i);
   });
 });
 
